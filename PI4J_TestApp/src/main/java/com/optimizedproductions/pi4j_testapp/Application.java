b545a7549a095e740fbbf038bc7645c041a2e401
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-02-28 17:31:04 -0600

    fixed project order

--- PI4J_TestApp/src/main/java/com/optimizedproductions/pi4j_testapp/Application.java
+++ PI4J_TestApp/src/main/java/com/optimizedproductions/pi4j_testapp/Application.java
@@ -1,3 +1,5 @@
+package com.optimizedproductions.pi4j_testapp;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
